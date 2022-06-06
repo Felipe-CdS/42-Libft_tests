9eb693f8f8c2820744a902ccf526455b824b74e1
fcoutinh <felipe32santos@hotmail.com>
2022-06-06 13:47:32 -0300

    tests

--- test_memmove.c
+++ test_memmove.c
@@ -6,7 +6,7 @@
 /*   By: fcoutinh <felipe32santos@hotmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 09:00:00 by fcoutinh          #+#    #+#             */
-/*   Updated: 2022/06/03 12:34:28 by fcoutinh         ###   ########.fr       */
+/*   Updated: 2022/06/06 13:31:12 by fcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,27 @@
 
 #define TESTS_QUANT 2
 
+//  1) Basic Test
 static int	test_1(){
-	return (ND);
+ 	//int			offset;
+ 	char	*target = "Hello World";
+ 	char	real_src[SIZE];
+	char	real_dst[SIZE];
+ 	char	test_src[SIZE];
+	char	test_dst[SIZE];
+
+ 	//offset = 6;
+ 	ft_strlcpy(real_src, target, 12);
+ 	ft_strlcpy(test_src, target, 12);
+ 	// real_dst = real_src + offset;
+ 	// test_dst = test_src + offset;
+ 	memmove(	real_dst, real_src, SIZE);
+	ft_memmove(	test_dst, test_src, SIZE);
+
+ 	printf("%s || %s\t", real_src, test_src);
+ 	if(memcmp(real_dst, test_dst, 12) || memcmp(real_src, test_src, 12)) return (KO);
+ 	return (OK);
+
 }
 
 static int	test_2(){
@@ -29,26 +48,3 @@ void		test_memmove(){
 	t_func_array f_ptr[TESTS_QUANT] = {&test_1, &test_2};
 	tests_iterator(f_ptr, TESTS_QUANT);
 }
-
-// int test_memmove()
-// {
-// 	int			offset;
-// 	char 		target[SIZE] = "Hello World...";
-// 	char		real_src[SIZE], *real_dst;
-// 	char		test_src[SIZE], *test_dst;
-
-// 	// 1) Basic Test
-// 	//	>Test setup
-// 	offset = 6;
-// 	strcpy(real_src, target);
-// 	strcpy(test_src, target);
-// 	real_dst = real_src + offset;
-// 	test_dst = test_src + offset;
-// 	//	>Test exec
-// 	memmove(	real_dst, real_src, SIZE);
-// 	ft_memmove(	test_dst, test_src, SIZE);
-
-// 	printf("%s || %s\t %s || %s", real_src, real_dst, test_src, test_dst);
-// 	if(strcmp(real_dst, test_dst) || strcmp(real_src, test_src)) return (1);
-// 	return (1);
-// }
