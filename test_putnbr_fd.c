132359e4458e6c963b758206cdabc127a38f8843
fcoutinh <felipe32santos@hotmail.com>
2022-06-08 14:42:25 -0300

    tests putnbr done

--- test_putnbr_fd.c
+++ test_putnbr_fd.c
@@ -6,41 +6,55 @@
 /*   By: fcoutinh <felipe32santos@hotmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 09:27:41 by fcoutinh          #+#    #+#             */
-/*   Updated: 2022/06/06 12:00:48 by fcoutinh         ###   ########.fr       */
+/*   Updated: 2022/06/08 14:37:53 by fcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libtests.h"
 #include "libft.h"
 
+#define TESTS_QUANT 2
+
 static int	test_1(){
-	char	test_str[13];
-	char	real_str[13];
-	FILE	*test_fp;
-	FILE	*real_fp;
+	int entry;
+	int result;
+	char file_str[100];
+	FILE *test_fp;
 
-	real_fp= fopen(".fd_R3", "rw+t");
-	test_fp= fopen(".fd_T3", "rw+t");
+	entry = 1234;
+	test_fp = fopen(".fd_T4", "rw+t");
+	ft_putnbr_fd(entry, 16);
 
-	// Test exec	
-	write(14, "Hello World", 12); write(14, "\n", 1);
-	ft_putendl_fd("Hello World", 15);
-
-	// test check
-	fread(real_str, sizeof(char), 13, real_fp);
-	fread(test_str, sizeof(char), 13, test_fp);
+	// Test check
+	fread(file_str, sizeof(char), 5, test_fp);
+	result = ft_atoi(file_str);
 
 	fclose(test_fp);
-	fclose(real_fp);
-	
-	// printnmem(test_str, 13); // debug print
-	// printnmem(real_str, 13); // debug print
 
-	if(ft_memcmp(test_str, real_str, 13) != 1) return (KO);
+	if(entry != result) return (KO);
 	return (OK);
 }
 
+static int	test_2(){
+	int entry;
+	int result;
+	char file_str[100];
+	FILE *test_fp;
+
+	entry = INT_MIN;
+	test_fp = fopen(".fd_T5", "rw+t");
+	ft_putnbr_fd(entry, 17);
+
+	// Test check
+	fread(file_str, sizeof(char), 20, test_fp);
+	result = ft_atoi(file_str);
+
+	fclose(test_fp);
+
+	if(entry != result) return (KO);
+	return (OK);
+}
 void		test_putnbr_fd(){	
-	t_func_array f_ptr[1] = { &test_1 };
-	tests_iterator(f_ptr, 1);
+	t_func_array f_ptr[TESTS_QUANT] = { &test_1, &test_2 };
+	tests_iterator(f_ptr, TESTS_QUANT);
 }
