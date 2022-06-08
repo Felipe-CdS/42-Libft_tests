d92a81ef2dfcb3e08d2ed9e5a310ed4e4b183a30
fcoutinh <felipe32santos@hotmail.com>
2022-06-08 16:26:34 -0300

    test_substr done

--- test_substr.c
+++ test_substr.c
@@ -6,46 +6,70 @@
 /*   By: fcoutinh <felipe32santos@hotmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 14:00:25 by fcoutinh          #+#    #+#             */
-/*   Updated: 2022/06/08 15:18:11 by fcoutinh         ###   ########.fr       */
+/*   Updated: 2022/06/08 16:26:03 by fcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 #include "libtests.h"
 
-#define TESTS_QUANT 0 
+#define TESTS_QUANT 5
 
-void test_substr()
-{
-	t_func_array f_ptr[TESTS_QUANT] = { };
-	tests_iterator(f_ptr, TESTS_QUANT);
-
-	/*
-	char	*src;
+// 1) Basic test
+static int	test_1(){
+	char	*s1 = "Hello World";
 	char	*dst;
-
-	// 1) Basic test
-	src	= "Hello World";
-	dst = ft_substr(src, 6,	5);
-	if(strcmp(dst, "World"))	return (1);
-	free(dst);
-		
-	// 2) Empty src
-	src = "";
-	dst = ft_substr(src, 0,	6);
-	if(dst != NULL)				return (2);
-	free(dst);
 	
-	// 3) len = 0
-	src = "Hello World";
-	dst = ft_substr(src, 6,	0);
-	if(dst != NULL)				return (3);
-	free(dst);
+	dst = ft_substr(s1, 0, 11);
 
-	// 4) max len > src size
-	src = "Hello World";
-	dst = ft_substr(src, 6,	100);
-	if(strcmp(dst, "World"))	return (4);
-	free(dst);
-	*/
+	if(strcmp(dst, "Hello World") != 0)	return (KO);
+	return (OK);
 }
 
+// 2) Basic test
+static int	test_2(){
+	char	*s1 = "Hello World";
+	char	*dst;
+	
+	dst = ft_substr(s1, 6, 5);
+
+	if(strcmp(dst, "World") != 0)	return (KO);
+	return (OK);
+}
+
+// 3) LEN == 0
+static int	test_3(){
+	char	*s1 = "Hello World";
+	char	*dst;
+	
+	dst = ft_substr(s1, 6, 0);
+
+	if(strcmp(dst, "") != 0)	return (KO);
+	return (OK);
+}
+
+// 4) src empty
+static int	test_4(){
+	char	*s1 = "";
+	char	*dst;
+	
+	dst = ft_substr(s1, 6, 6);
+
+	if(strcmp(dst, "") != 0)	return (KO);
+	return (OK);
+}
+
+// 5) max len > src size
+static int	test_5(){
+	char	*s1 = "Hello World";
+	char	*dst;
+	
+	dst = ft_substr(s1, 6, 100);
+
+	if(strcmp(dst, "World") != 0)	return (KO);
+	return (OK);
+}
+void test_substr()
+{
+	t_func_array f_ptr[TESTS_QUANT] = { &test_1, &test_2, &test_3, &test_4, &test_5 };
+	tests_iterator(f_ptr, TESTS_QUANT);
+}
