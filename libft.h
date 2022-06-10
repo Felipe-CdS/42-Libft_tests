9e180de989be7da380b9214253b129a0f58359c3
fcoutinh <felipe32santos@hotmail.com>
2022-06-10 13:18:41 -0300

    strtrim tests

--- libft.h
+++ libft.h
@@ -6,57 +6,57 @@
 /*   By: fcoutinh <felipe32santos@hotmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 11:05:40 by fcoutinh          #+#    #+#             */
-/*   Updated: 2022/06/08 17:04:38 by fcoutinh         ###   ########.fr       */
+/*   Updated: 2022/06/10 13:09:34 by fcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
-#define LIBFT_H
-#include <unistd.h>
-#include <stdlib.h>
+# define LIBFT_H
+# include <unistd.h>
+# include <stdlib.h>
 
-typedef struct		s_list
+typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
 
-int		ft_isalpha(int ch);													// Done
-int		ft_isdigit(int c); 													// Done
-int		ft_isalnum(int c); 													// Done
-int		ft_isascii(int c); 													// Done
-int		ft_isprint(int c); 													// Done
-int		ft_toupper(int c); 													// Done
-int		ft_tolower(int c); 													// Done
-int		ft_strncmp(const char *s1, const char *s2, size_t n); 				// Done
-int		ft_memcmp(const void *s1, const void *s2, size_t n); 				// Done
-int		ft_atoi(const char *str); 											// Done
+int		ft_isalpha(int ch);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_atoi(const char *str);
 
-size_t	ft_strlen(const char *s); 											// Done
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize); 			// Done
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize); 			// Done
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
-void	ft_putchar_fd(char c, int fd); 										// Done
-void	ft_putstr_fd(char *s, int fd); 										// Done
-void	ft_putendl_fd(char *s, int fd); 									// Done
-void	ft_putnbr_fd(int n, int fd); 										// Done
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));				// XXXX
-void	ft_bzero(void *s, size_t n); 										// Done
-void	*ft_calloc(size_t count, size_t size);								// XXXX
-void	*ft_memset(void *b, int c, size_t len); 							// Done 
-void	*ft_memcpy(void *dst, const void *src, size_t n); 					// Done
-void	*ft_memmove(void *dst, const void *src, size_t len);				// XXXX
-void	*ft_memchr(const void *s, int c, size_t n); 						// Done
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));				
+void	ft_bzero(void *s, size_t n);	
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);				
 
-char	*ft_strchr(const char *s, int c); 									// Done
-char	*ft_strrchr(const char *s, int c); 									// Done
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len); 	// Done
-char	*ft_strdup(const char *s1); 										// Done
-char	*ft_substr(char const *s, unsigned int start, size_t len); 			// Done
-char	*ft_strjoin(char const *s1, char const *s2); 						// Done
-char	*ft_strtrim(char const *s1, char const *set);						// XXXX
-char	*ft_itoa(int n); 													// Done
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));			// XXXX
-char	**ft_split(char	const *s, char c);									// XXXX
+char	*ft_strchr(const char *s, int c);							
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);			
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	**ft_split(char	const *s, char c);			
 
 // Bonus
 int		ft_lstsize(t_list *lst);
--- test_strtrim.c
+++ test_strtrim.c
@@ -6,23 +6,73 @@
 /*   By: fcoutinh <felipe32santos@hotmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 17:20:47 by fcoutinh          #+#    #+#             */
-/*   Updated: 2022/06/08 17:21:57 by fcoutinh         ###   ########.fr       */
+/*   Updated: 2022/06/10 13:17:57 by fcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "libtests.h"
 
-#define TESTS_QUANT 1
+#define TESTS_QUANT 5
 
 static int	test_1(){
-	return (ND);
-	if(/*original_func_ret != test_func_ret*/1) return (KO);
+	char *entry = "Hello World!";
+	char *real = "H Wrd!";
+	char *test;
+	
+	test = ft_strtrim(entry, "elo");
+
+	if(strcmp(real, test) != 0) return (KO);
 	return (OK);
 }
 
+static int	test_2(){
+	char *entry	= "Hello World!";
+	char *real	= "Hello World!";
+	char *test;
+	
+	test = ft_strtrim(entry, "j");
+
+	if(strcmp(real, test) != 0) return (KO);
+	return (OK);
+}
+
+static int	test_3(){
+	char *entry	= "Hello World!";
+	char *real	= "Hello World!";
+	char *test;
+	
+	test = ft_strtrim(entry, "");
+
+	if(strcmp(real, test) != 0) return (KO);
+	return (OK);
+}
+
+static int	test_4(){
+	char *entry	= "Hello World!       ";
+	char *real	= "HelloWorld!";
+	char *test;
+	
+	test = ft_strtrim(entry, " ");
+
+	if(strcmp(real, test) != 0) return (KO);
+	return (OK);
+}
+
+static int	test_5(){
+	char *entry	= "";
+	char *real	= "";
+	char *test;
+	
+	test = ft_strtrim(entry, "abc");
+
+	if(strcmp(real, test) != 0) return (KO);
+	return (OK);
+}
+
+
 void		test_strtrim(){	
-	t_func_array f_ptr[TESTS_QUANT] = { &test_1 };
+	t_func_array f_ptr[TESTS_QUANT] = { &test_1, &test_2, &test_3, &test_4, &test_5 };
 	tests_iterator(f_ptr, TESTS_QUANT);
 }
 
