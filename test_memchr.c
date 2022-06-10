69d39e4d36bffc7bb43ab6e9d965cd3fde730813
Felipe-CdS <felipe32santos@hotmail.com>
2022-06-09 23:39:58 -0300

    memchr & strchr new tests

--- test_memchr.c
+++ test_memchr.c
@@ -13,111 +13,112 @@
 #include "libft.h"
 #include "libtests.h"
 
-#define TESTS_QUANT 7
+#define TESTS_QUANT 10
 
-#define CHECK_COND memchr(entry, search, size) != ft_memchr(entry, search, size)
+#define CMP_CHECK memchr(entry, search, size) != ft_memchr(entry, search, size)
 
 // Char at first index
-static int	test_1(){		
-	return (ND);
+static int	test_1(){
+	char			*entry = "abcde12345";
+	char			search = 'a';
+	unsigned int	size = ft_strlen(entry);
+
+	if(CMP_CHECK)	return (KO);
+	return (OK);
 }
 
 // Char at random index
-static int	test_2(){		
-	return (ND);
+static int	test_2(){
+	char			*entry = "abcde12345";
+	char			search = 'e';
+	unsigned int	size = ft_strlen(entry);
+
+	if(CMP_CHECK)	return (KO);
+	return (OK);
 }
 
 // Char at last index
-static int	test_3(){		
-	return (ND);
+static int	test_3(){
+	char			*entry = "abcde12345";
+	char			search = '5';
+	unsigned int	size = ft_strlen(entry);
+
+	if(CMP_CHECK)	return (KO);
+	return (OK);
 }
 
 // Char is a space
-static int	test_4(){		
-	return (ND);
+static int	test_4(){
+	char			*entry = "abc d12345";
+	char			search = ' ';
+	unsigned int	size = ft_strlen(entry);
+
+	if(CMP_CHECK)	return (KO);
+	return (OK);
 }
 
-// Char is a space
-static int	test_5(){		
-	return (ND);
+// search in an entry with duplicates
+static int	test_5(){
+	char			*entry = "aabbccddee";
+	char			search = 'c';
+	unsigned int	size = ft_strlen(entry);
+
+	if(CMP_CHECK)	return (KO);
+	return (OK);
+
 }
 
 // Char isn't at src
-static int	test_6(){		
-	return (ND);
+static int	test_6(){
+	char			*entry = "abcde12345";
+	char			search = 'f';
+	unsigned int	size = ft_strlen(entry);
+
+	if(CMP_CHECK)	return (KO);
+	return (OK);
 }
 
 // Src is empty
 static int	test_7(){
-	return (ND);
+	char			*entry = "";
+	char			search = 'f';
+	unsigned int	size = ft_strlen(entry);
+
+	if(CMP_CHECK)	return (KO);
+	return (OK);
+}
+
+// Char is non printable
+static int	test_8(){
+	char			entry[100] = {'H', 'e', 'l', 'l', 'o', '\1', '\2', '\3', '\0'};
+	char			search = '\0';
+	unsigned int	size = ft_strlen(entry);
+
+	if(CMP_CHECK)	return (KO);
+	return (OK);
+}
+
+// char is hidden
+static int	test_9(){
+	char			entry[100] = {'A', 'B', '\0', 'C', 'D', 'E'};
+	char			search = 'E';
+	unsigned int	size = 6;
+
+	if(CMP_CHECK)	return (KO);
+	return (OK);
+}
+
+// negative value
+static int	test_10(){
+	int			entry[8] = {1, 2, 3, 0 , 42, -14, 2, 8};
+	char			search = -14;
+	unsigned int	size = 8;
+
+	if(CMP_CHECK)	return (KO);
+	return (OK);
 }
 
 void	test_memchr(){	
-	t_func_array f_ptr[TESTS_QUANT] = {&test_1, &test_2, &test_3, &test_4, &test_5, &test_6, &test_7};
+	t_func_array f_ptr[TESTS_QUANT] = {&test_1, &test_2, &test_3, &test_4, &test_5, &test_6, &test_7, &test_8, &test_9, &test_10};
 	tests_iterator(f_ptr, TESTS_QUANT);
 }
-
-// int	test_memchr()
-// {
-// 	char		*entry;
-// 	char		search;
-// 	char		sp_c1[100] = {'H', 'e', 'l', 'l', 'o', '\1', '\2', '\3', '\0'};
-// 	char		sp_c2[100];
-// 	unsigned int	size;
-
-// 	// search at first index
-// 	entry	= "abcde12345";
-// 	search 	= 'a';
-// 	size	= ft_strlen(entry);
-// 	if(memchr(entry, search, size) != ft_memchr(entry, search, size))	return (1);
-
-// 	// search at last index
-// 	entry	= "abcde12345";
-// 	search 	= '5';
-// 	size	= ft_strlen(entry);
-// 	if(memchr(entry, search, size) != ft_memchr(entry, search, size))	return (2);
-
-// 	// search at random index
-// 	entry	= "abcde12345";
-// 	search 	= 'e';
-// 	size	= ft_strlen(entry);
-// 	if(memchr(entry, search, size) != ft_memchr(entry, search, size))	return (3);
-
-// 	// search isn't alnum
-// 	entry	= "a bcde1234";
-// 	search 	= ' ';
-// 	size	= ft_strlen(entry);
-// 	if(memchr(entry, search, size) != ft_memchr(entry, search, size))	return (4);
-
-// 	// search in an entry with duplicates
-// 	entry	= "aabbccddee";
-// 	search 	= 'c';
-// 	size	= ft_strlen(entry);
-// 	if(memchr(entry, search, size) != ft_memchr(entry, search, size))	return (5);
-
-// 	// char isn't at src
-// 	entry	= "abcde12345";
-// 	search 	= 'f';
-// 	size	= ft_strlen(entry);
-// 	if(memchr(entry, search, size) != ft_memchr(entry, search, size))	return (6);
-
-// 	// src is empty
-// 	entry	= "";
-// 	search 	= 'f';
-// 	size	= ft_strlen(entry);
-// 	if(memchr(entry, search, size) != ft_memchr(entry, search, size))	return (7);
-
-// 	// char is non printable
-// 	//char	sp_c1[100] = {'H', 'e', 'l', 'l', 'o', '\1', '\2', '\3', '\0'};
-// 	search 	= '\1';
-// 	size	= ft_strlen(sp_c1);
-// 	if(memchr(sp_c1, search, size) != ft_memchr(sp_c1, search, size))	return (8);
-
-// 	// src isn't initialized
-// 	//char	sp_c2[100];
-// 	search 	= 'a';
-// 	size	= ft_strlen(sp_c2);
-// 	if(memchr(sp_c2, search, size) != ft_memchr(sp_c2, search, size))	return (9);
-
-// 	return (0);
-// }
--- test_strchr.c
+++ test_strchr.c
@@ -12,67 +12,84 @@
 #include "libft.h"
 #include "libtests.h"
 
-#define TESTS_QUANT 7
+#define TESTS_QUANT 9
 
-#define CHECK_COND strchr(entry, search) != ft_strchr(entry, search)
+#define CMP_CHECK strchr(entry, search) != ft_strchr(entry, search)
 
 // Char at first index
-static int	test_1(){		
-	char	*entry = "abcde12345";
-	char	search = 'a';
-	if(CHECK_COND)	return (KO);
+static int	test_1(){
+	char			*entry = "abcde12345";
+	char			search = 'a';
+	if(CMP_CHECK)	return (KO);
 	return (OK);
 }
 
 // Char at random index
-static int	test_2(){		
-	char	*entry = "abcde12345";
-	char	search = '2';
-	if(CHECK_COND)	return (KO);
+static int	test_2(){
+	char			*entry = "abcde12345";
+	char			search = 'e';
+	if(CMP_CHECK)	return (KO);
 	return (OK);
 }
 
 // Char at last index
-static int	test_3(){		
-	char	*entry = "abcde12345";
-	char	search = '5';
-	if(CHECK_COND)	return (KO);
+static int	test_3(){
+	char			*entry = "abcde12345";
+	char			search = '5';
+	if(CMP_CHECK)	return (KO);
 	return (OK);
 }
 
 // Char is a space
-static int	test_4(){		
-	char	*entry = "a b c 1 2 3";
-	char	search = ' ';
-	if(CHECK_COND)	return (KO);
+static int	test_4(){
+	char			*entry = "abc d12345";
+	char			search = ' ';
+	if(CMP_CHECK)	return (KO);
 	return (OK);
 }
 
-// Char is a space
-static int	test_5(){		
-	char	entry[100]	= {'1', 'a', '\2', '\1', '\0'};
-	char	search		= 1;
-	if(CHECK_COND)	return (KO);
+// search in an entry with duplicates
+static int	test_5(){
+	char			*entry = "aabbccddee";
+	char			search = 'c';
+	if(CMP_CHECK)	return (KO);
 	return (OK);
+
 }
 
 // Char isn't at src
-static int	test_6(){		
-	char	*entry	= "* - + ! | @";
-	char	search	= 'a';
-	if(CHECK_COND)	return (KO);
+static int	test_6(){
+	char			*entry = "abcde12345";
+	char			search = 'f';
+	if(CMP_CHECK)	return (KO);
 	return (OK);
 }
 
 // Src is empty
-static int	test_7(){		
-	char	*entry	= "";
-	char	search	= 'a';
-	if(CHECK_COND)	return (KO);
+static int	test_7(){
+	char			*entry = "";
+	char			search = 'f';
+	if(CMP_CHECK)	return (KO);
+	return (OK);
+}
+
+// Char is non printable
+static int	test_8(){
+	char			entry[100] = {'H', 'e', 'l', 'l', 'o', '\1', '\2', '\3', '\0'};
+	char			search = '\1';
+	if(CMP_CHECK)	return (KO);
+	return (OK);
+}
+
+// char is hidden
+static int	test_9(){
+	char			entry[100] = {'A', 'B', '\0', 'C', 'D', 'E'};
+	char			search = 'E';
+	if(CMP_CHECK)	return (KO);
 	return (OK);
 }
 
 void	test_strchr(){	
-	t_func_array f_ptr[TESTS_QUANT] = {&test_1, &test_2, &test_3, &test_4, &test_5, &test_6, &test_7};
+	t_func_array f_ptr[TESTS_QUANT] = {&test_1, &test_2, &test_3, &test_4, &test_5, &test_6, &test_7, &test_8, &test_9};
 	tests_iterator(f_ptr, TESTS_QUANT);
 }
