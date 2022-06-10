e63b109d4b245fefc961d7b8657f5aeaf85456e3
fcoutinh <felipe32santos@hotmail.com>
2022-06-10 15:29:32 -0300

    malloc tests

--- test_calloc.c
+++ test_calloc.c
@@ -6,39 +6,62 @@
 /*   By: fcoutinh <felipe32santos@hotmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 12:54:18 by fcoutinh          #+#    #+#             */
-/*   Updated: 2022/06/03 14:07:54 by fcoutinh         ###   ########.fr       */
+/*   Updated: 2022/06/10 15:28:16 by fcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 #include "libtests.h"
 
-#define TESTS_QUANT 1
+#define TESTS_QUANT 3
 
 static int	test_1(){
-	return (ND);
+	unsigned long i;
+	int 	size;	
+	char	*buffer;
+
+	i = 0;
+	size = 10;
+	buffer = (char *) ft_calloc(size, sizeof(char));
+	while(i < size * sizeof(char))
+	{
+		if(buffer[i] != '\0') return (KO);
+		i++;
+	}
+	return (OK);
 }
 
+static int	test_2(){
+	unsigned long i;
+	int 	size;	
+	char	*buffer;
+
+	i = 0;
+	size = 10;
+	buffer = (char *) ft_calloc(size, sizeof(int));
+	while(i < size * sizeof(int))
+	{
+		if(buffer[i] != '\0') return (KO);
+		i++;
+	}
+	return (OK);
+}
+
+static int	test_3(){
+	unsigned long i;
+	int 	size;	
+	char	*buffer;
+
+	i = 0;
+	size = 0;
+	buffer = (char *) ft_calloc(size, sizeof(int));
+	while(i < size * sizeof(int))
+	{
+		if(buffer[i] != '\0') return (KO);
+		i++;
+	}
+	return (OK);
+}
 void	test_calloc(){	
-	t_func_array f_ptr[TESTS_QUANT] = {&test_1};
+	t_func_array f_ptr[TESTS_QUANT] = {&test_1, &test_2, &test_3};
 	tests_iterator(f_ptr, TESTS_QUANT);
 }
-
-// int	test_calloc()
-// {
-// 	// int 	i;
-// 	// char *buffer;
-
-// 	// i = 0;
-// 	// buffer = (char *) ft_calloc(10, sizeof(char));
-// 	// if(!buffer) printf("Null buffer");
-
-// 	// /*
-// 	// while(i < 10)
-// 	// {
-// 	// 	printf("%d\n", buffer[i]);
-// 	// 	i++;
-// 	// }
-// 	// */
-// 	// free(buffer);
-// 	return (0);
-// }
