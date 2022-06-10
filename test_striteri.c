82b5f5bb8de23b3a5d610879beaf55a6b048107f
fcoutinh <felipe32santos@hotmail.com>
2022-06-10 14:47:27 -0300

    striteri tests

--- test_striteri.c
+++ test_striteri.c
@@ -6,7 +6,7 @@
 /*   By: fcoutinh <felipe32santos@hotmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 17:24:28 by fcoutinh          #+#    #+#             */
-/*   Updated: 2022/06/08 17:24:41 by fcoutinh         ###   ########.fr       */
+/*   Updated: 2022/06/10 14:46:32 by fcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,22 @@
 
 #define TESTS_QUANT 1
 
+void		test_function(unsigned int index,  char *char_ptr)
+{
+	char iteration_result;
+
+	iteration_result = ft_toupper(char_ptr[index]);
+	ft_memmove(&char_ptr[index], &iteration_result, 1);
+}
+
+
 static int	test_1(){
-	return (ND);
-	if(/*original_func_ret != test_func_ret*/1) return (KO);
+	char entry[100] = "Hello World!";
+	char *real		= "HELLO WORLD!";
+
+	ft_striteri(entry, &test_function);
+
+	if(strcmp(entry, real) != 0) return (KO);
 	return (OK);
 }
 
